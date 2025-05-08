3eccf41de99f45f3999db54705a725624c00b6fd
jaspergass <115849451+jaspergass@users.noreply.github.com>
2025-05-08 16:18:38 -0700

    Revert "about page testing"
    This reverts commit b4ad88f7832adc1a718401a1e74cf4fe62be0b69.

--- js/about-page.js
+++ js/about-page.js
@@ -31,77 +31,37 @@
 //     }
 //   }
 
-// function toggleAboutContent(button) {
-//     const section = button.closest(".collapsible-section");
-//     const box = section.querySelector(".about-box");
-//     const icon = button.querySelector("img");
+function toggleAboutContent(button) {
+    const section = button.closest(".collapsible-section");
+    const box = section.querySelector(".about-box");
+    const icon = button.querySelector("img");
   
-//     if (box.classList.contains("expanded")) {
-//       box.style.height = box.scrollHeight + "px";
-//       requestAnimationFrame(() => {
-//         box.style.height = "0px";
-//         box.style.padding = "0 1em";
-//         box.classList.remove("expanded");
-//         icon.classList.remove("rotated");
+    if (box.classList.contains("expanded")) {
+      box.style.height = box.scrollHeight + "px";
+      requestAnimationFrame(() => {
+        box.style.height = "0px";
+        box.style.padding = "0 1em";
+        box.classList.remove("expanded");
+        icon.classList.remove("rotated");
       
-//         // After transition ends, THEN remove border-bottom
-//         box.addEventListener("transitionend", function handler(e) {
-//           if (e.propertyName === "height") {
-//             box.style.borderBottom = "0em";
-//             box.removeEventListener("transitionend", handler);
-//           }
-//         });
-//       });
-//     } else {
-//       box.style.height = box.scrollHeight + "px";
-//       box.classList.add("expanded");
-//       box.style.padding = "1em";
-//       box.style.borderBottom = "0.09em solid black";
-//       icon.classList.add("rotated");
-  
-//       box.addEventListener("transitionend", function handler() {
-//         box.style.height = "auto";
-//         box.removeEventListener("transitionend", handler);
-//       });
-//     }
-//   }
-
-function toggleAboutContent(trigger) {
-  const section = trigger.closest(".collapsible-section");
-  const box = section.querySelector(".aboutContent");
-  const icon = section.querySelector(".star-icon");
-
-  const isExpanded = box.classList.contains("expanded");
-
-  if (isExpanded) {
-    box.style.height = box.scrollHeight + "px"; // set explicit height
-    requestAnimationFrame(() => {
-      box.style.height = "0px";
-      box.style.padding = "0 1em";
-      icon.classList.remove("rotated");
-      box.classList.remove("expanded");
-    });
-  } else {
-    box.style.display = "block";
-    const height = box.scrollHeight + "px";
-    box.style.height = "0px"; // reset to zero before expanding
-    box.classList.add("expanded");
-    icon.classList.add("rotated");
-    requestAnimationFrame(() => {
-      box.style.height = height;
+        // After transition ends, THEN remove border-bottom
+        box.addEventListener("transitionend", function handler(e) {
+          if (e.propertyName === "height") {
+            box.style.borderBottom = "0em";
+            box.removeEventListener("transitionend", handler);
+          }
+        });
+      });
+    } else {
+      box.style.height = box.scrollHeight + "px";
+      box.classList.add("expanded");
       box.style.padding = "1em";
-    });
-  }
-
-  // Clean up height after expansion
-  box.addEventListener("transitionend", function handler(e) {
-    if (e.propertyName === "height") {
-      if (box.classList.contains("expanded")) {
+      box.style.borderBottom = "0.09em solid black";
+      icon.classList.add("rotated");
+  
+      box.addEventListener("transitionend", function handler() {
         box.style.height = "auto";
-      } else {
-        box.style.display = "none"; // hide completely when collapsed
-      }
-      box.removeEventListener("transitionend", handler);
+        box.removeEventListener("transitionend", handler);
+      });
     }
-  });
-}
+  }
